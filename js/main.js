cf25970f10d71fc861fb318a4bb1330de921ebed
shagia <mrkraiola@gmail.com>
2019-05-22 18:54:15 -0700

    clean up css and js

--- js/main.js
+++ js/main.js
@@ -12,81 +12,81 @@ const navBar = document.getElementById("navBar")
 
 
 const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCjrJ9uA1brSWer7j14KBGqywYX63GkyenG6l79zbVirQWKsVSHK9cx6NKRnGhGkXQHPYV2o-HPdyQ/pub?output=csv"
-async function fetchData(url_){
-  const data = await fetch(url_)
-            .then(res => res.text()) // convert body stream to string text
-            .then(res => d3.csvParse(res)); //d3 csv parse string texts to array of objects
-  
-  return data;
+async function fetchData(url_) {
+    const data = await fetch(url_)
+        .then(res => res.text()) // convert body stream to string text
+        .then(res => d3.csvParse(res)); //d3 csv parse string texts to array of objects
+
+    return data;
 }
 
 
 
-async function init(){
-  csvData = await fetchData(url); // fetch data await for async function to continue
-  
-  for (let i = 0; i < csvData.length; i++){
+async function init() {
+    csvData = await fetchData(url); // fetch data await for async function to continue
+
+    for (let i = 0; i < csvData.length; i++) {
+
+        // Draws a node for each array in the csv, gets the relevant array key through the current iteration number
+        /******
+        No Neeed to declare generateIcon function inside a for loop,
+        othewise for each iteration you will be spending computing redeclaring a function,
+        deafeating the purporse of code reuse, i've moved it away, plus 
     
-    // Draws a node for each array in the csv, gets the relevant array key through the current iteration number
-    /******
-    No Neeed to declare generateIcon function inside a for loop,
-    othewise for each iteration you will be spending computing redeclaring a function,
-    deafeating the purporse of code reuse, i've moved it away, plus 
+        your original function you pass i as parameter key, but then use csvData[i] which is not
+        delcared in the scope of generateIcon.
     
-    your original function you pass i as parameter key, but then use csvData[i] which is not
-    delcared in the scope of generateIcon.
-    
-    What you probably want to do is to generateIcon(csvData[i]) passing the current csvRowEntry row you're
-    interating over and the index for the key as you've design
-     
-     you can add the addEventListener on your generate Icon too
-    *******/
-    generateIcon(csvData[i], i)
-  }
-  
-  // Gets the children of navbar
-  const navBarItems = document.getElementById("navBar").getElementsByClassName("iconContainer")
-  const iconImgItems = document.getElementById("navBar").getElementsByClassName("iconImg")
-  // Converts the children to an array because HTMLcollection is not a REAL array
-  const navBarItemsArray = Array.from(navBarItems)
-  console.log(navBarItemsArray);
-  /*const iconImgItemsArray = Array.prototype.slice.call()*/
-
-  /* remove window load , eventlistener is on generteIcon */
-  
-}
-init();
-    
-
-function generateIcon(csvRowEntry, key){
-     // gets csv row and key an generate icons
-      var newAnchor = document.createElement('a')
-      newAnchor.className = 'iconContainer iconImg';
-      newAnchor.href = "#";
-      newAnchor.dataset.number = key
-
-      var imgNode = document.createElement('img')
-      imgNode.src = "https://picsum.photos/100/100"
-      imgNode.className = 'iconImgSrc'
-
-      var textNode = document.createTextNode(csvRowEntry.Name);
-
-      newAnchor.appendChild(imgNode);
-      newAnchor.appendChild(textNode);
-      newAnchor.addEventListener('click', navClick); // add event in each a link, call navClick
-      navBarSource.appendChild(newAnchor);
+        What you probably want to do is to generateIcon(csvData[i]) passing the current csvRowEntry row you're
+        interating over and the index for the key as you've design
+         
+         you can add the addEventListener on your generate Icon too
+        *******/
+        generateIcon(csvData[i], i)
     }
 
-function navClick(event){
-  //**this** is the dom scope for the element the event is coming
-  //therefore you don't need to select it, **this** is the selection
-  const dataNumber = this.dataset.number;
-  console.log("coming from: ", dataNumber);
-  currSelect = dataNumber;
-  updateStateDOM(dataNumber);
+    // Gets the children of navbar
+    const navBarItems = document.getElementById("navBar").getElementsByClassName("iconContainer")
+    const iconImgItems = document.getElementById("navBar").getElementsByClassName("iconImg")
+    // Converts the children to an array because HTMLcollection is not a REAL array
+    const navBarItemsArray = Array.from(navBarItems)
+    console.log(navBarItemsArray);
+    /*const iconImgItemsArray = Array.prototype.slice.call()*/
+
+    /* remove window load , eventlistener is on generteIcon */
+
+}
+init();
+
+
+function generateIcon(csvRowEntry, key) {
+    // gets csv row and key an generate icons
+    var newAnchor = document.createElement('a')
+    newAnchor.className = 'iconContainer iconImg';
+    newAnchor.href = "#";
+    newAnchor.dataset.number = key
+
+    var imgNode = document.createElement('img')
+    imgNode.src = "https://picsum.photos/100/100"
+    imgNode.className = 'iconImgSrc'
+
+    var textNode = document.createTextNode(csvRowEntry.Name);
+
+    newAnchor.appendChild(imgNode);
+    newAnchor.appendChild(textNode);
+    newAnchor.addEventListener('click', navClick); // add event in each a link, call navClick
+    navBarSource.appendChild(newAnchor);
 }
 
-      
+function navClick(event) {
+    //**this** is the dom scope for the element the event is coming
+    //therefore you don't need to select it, **this** is the selection
+    const dataNumber = this.dataset.number;
+    console.log("coming from: ", dataNumber);
+    currSelect = dataNumber;
+    updateStateDOM(dataNumber);
+}
+
+
 function updateStateDOM(ele) {
 
     console.log(this)
@@ -96,16 +96,16 @@ function updateStateDOM(ele) {
     const sectionDesc = document.getElementsByClassName("sectionDescription")
     const runnerDesc = document.getElementsByClassName("sectionDescription1")
     const testName = "Tom"
-    
+
     sideA.style.display = 'block'
 
-        sideA.getElementsByClassName("runnerName")[0].innerHTML = csvData[ele].Name
-        sideA.getElementsByClassName("runnerDetails")[0].innerHTML = csvData[ele].Description
-        sideA.getElementsByClassName("sectionDescription1")[0].innerHTML = csvData[ele].Education
-        sideA.getElementsByClassName("sectionDescription2")[0].innerHTML = csvData[ele].Health
-        sideA.getElementsByClassName("sectionDescription3")[0].innerHTML = csvData[ele].Immigration
-        sideA.getElementsByClassName("sectionDescription4")[0].innerHTML = csvData[ele].Civil
-        sideA.getElementsByClassName("sectionDescription5")[0].innerHTML = csvData[ele].Environment
-        sideA.getElementsByClassName("sectionDescription6")[0].innerHTML = csvData[ele].Gun_Control
+    sideA.getElementsByClassName("runnerName")[0].innerHTML = csvData[ele].Name
+    sideA.getElementsByClassName("runnerDetails")[0].innerHTML = csvData[ele].Description
+    sideA.getElementsByClassName("sectionDescription1")[0].innerHTML = csvData[ele].Education
+    sideA.getElementsByClassName("sectionDescription2")[0].innerHTML = csvData[ele].Health
+    sideA.getElementsByClassName("sectionDescription3")[0].innerHTML = csvData[ele].Immigration
+    sideA.getElementsByClassName("sectionDescription4")[0].innerHTML = csvData[ele].Civil
+    sideA.getElementsByClassName("sectionDescription5")[0].innerHTML = csvData[ele].Environment
+    sideA.getElementsByClassName("sectionDescription6")[0].innerHTML = csvData[ele].Gun_Control
 
 }
