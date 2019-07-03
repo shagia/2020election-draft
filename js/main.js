926f0628ea7eaa16d513df01e49f67a8a3a5a26b
shagia <mrkraiola@gmail.com>
2019-07-02 18:28:09 -0700

    main.js cleanup

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-/* global d3, addeventlistenerlist, updatestatedom */
-
+//TODO: Move to React soon
 var csvData = []
 const navBarSource = document.getElementById('navBar');
-/* The states let describes what's being shown on the screen. The first state, 0, is for the home screen, the second state, 1, is for the single column runner screen, and the third state, 2, is for the dual column runner screen. */
-let states = [true, false, false]
 // Runners currently selected
 var currSelect = []
 var currLocation = [];
@@ -11,9 +8,13 @@ var rawLocation = [];
 //Tool to split location coordinates into an array for Mapbox
 const splitFunc = location => location.split(", ");
 
-// Gets the parent navbar
-const navBar = document.getElementById("navBar")
-
+//Mapbox interaction
+function flyToLocation(coords) {
+    map.flyTo({
+        center: (coords),
+        zoom: 9
+    });
+}
 
 const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCjrJ9uA1brSWer7j14KBGqywYX63GkyenG6l79zbVirQWKsVSHK9cx6NKRnGhGkXQHPYV2o-HPdyQ/pub?output=csv"
 async function fetchData(url_) {
@@ -22,55 +23,19 @@ async function fetchData(url_) {
         .then(res => d3.csvParse(res)); //d3 csv parse string texts to array of objects
 
     return data;
-}   
-
+}
 
 async function init() {
     csvData = await fetchData(url); // fetch data await for async function to continue
-
     for (let i = 0; i < csvData.length; i++) {
-
         // Draws a node for each array in the csv, gets the relevant array key through the current iteration number
-        /******
-        No Neeed to declare generateIcon function inside a for loop,
-        othewise for each iteration you will be spending computing redeclaring a function,
-        deafeating the purporse of code reuse, i've moved it away, plus 
-    
-        your original function you pass i as parameter key, but then use csvData[i] which is not
-        delcared in the scope of generateIcon.
-    
-        What you probably want to do is to generateIcon(csvData[i]) passing the current csvRowEntry row you're
-        interating over and the index for the key as you've design
-         
-         you can add the addEventListener on your generate Icon too
-        *******/
         generateIcon(csvData[i], i)
     }
-
-    // Gets the children of navbar
-    const navBarItems = document.getElementById("navBar").getElementsByClassName("iconContainer")
-    const iconImgItems = document.getElementById("navBar").getElementsByClassName("iconImg")
-    // Converts the children to an array because HTMLcollection is not a REAL array
-    const navBarItemsArray = Array.from(navBarItems)
-    console.log(navBarItemsArray);
-    /*const iconImgItemsArray = Array.prototype.slice.call()*/
-
-    /* remove window load , eventlistener is on generteIcon */
-
 }
 init();
 
-function flyToLocation(coords) {
-  map.flyTo({
-    center: (coords),
-    zoom: 9
-  });
-}
-
-
-
 function generateIcon(csvRowEntry, key) {
-    // gets csv row and key an generate icons
+    // gets csv row and key and generates icons
     var newAnchor = document.createElement('a')
     newAnchor.className = 'iconContainer iconImg';
     newAnchor.href = "#";
@@ -92,12 +57,11 @@ function generateIcon(csvRowEntry, key) {
 }
 
 function navClick(event) {
-    //**this** is the dom scope for the element the event is coming
-    //therefore you don't need to select it, **this** is the selection
+    //All icons now refer to this function, therefore no need to call the children of a DOM object
     const dataNumber = this.dataset.number;
     console.log("coming from: ", dataNumber);
 
-    function grabLocation(ele){
+    function grabLocation(ele) {
         rawLocation = csvData[dataNumber].Location
         console.log(rawLocation);
 
@@ -114,16 +78,13 @@ function navClick(event) {
 
 
 function updateStateDOM(ele) {
-
     console.log(this)
-
     var sideA = document.getElementById("infoSideA")
     var sideIntro = document.getElementById("infoIntro")
     const runnerDeets = document.getElementsByClassName("runnerName")
     const sectionDesc = document.getElementsByClassName("sectionDescription")
     const runnerDesc = document.getElementsByClassName("sectionDescription1")
     const runnerPic = document.getElementById("headShotSrc")
-    const testName = "Tom"
 
     sideA.style.display = 'block'
     sideIntro.style.display = 'none'
