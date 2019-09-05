86e34efe812c5c42a41e5ac4ec8aa8ec3fdd491b
shagia <mrkraiola@gmail.com>
2019-09-04 17:23:47 -0700

    cleaning mapbox file

--- js/mapbox.js
+++ js/mapbox.js
@@ -19,7 +19,6 @@ var matches = query.match(/^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\
 if (!matches) {
 return null;
 }
- 
 function coordinateFeature(lng, lat) {
 return {
 center: [lng, lat],
@@ -33,34 +32,21 @@ properties: {},
 type: 'Feature'
 };
 }
- 
 var coord1 = Number(matches[1]);
 var coord2 = Number(matches[2]);
 var geocodes = [];
- 
 if (coord1 < -90 || coord1 > 90) {
 // must be lng, lat
 geocodes.push(coordinateFeature(coord1, coord2));
 }
- 
 if (coord2 < -90 || coord2 > 90) {
 // must be lat, lng
 geocodes.push(coordinateFeature(coord2, coord1));
 }
- 
 if (geocodes.length === 0) {
 // else could be either lng, lat or lat, lng
 geocodes.push(coordinateFeature(coord1, coord2));
 geocodes.push(coordinateFeature(coord2, coord1));
 }
- 
 return geocodes;
 };
- 
-/*map.addControl(new MapboxGeocoder({
-accessToken: mapboxgl.accessToken,
-localGeocoder: coordinatesGeocoder,
-zoom: 4,
-placeholder: "Try: -40, 170",
-mapboxgl: mapboxgl
-}));*/
